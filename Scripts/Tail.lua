c3b88994a5e6f809d1e6bc8d13ccc4c1eddb7b09
Kihira <kihira@kihirakreations.co.uk>
2016-01-19 20:08:40 +0000

    Attempt at getting animation to work

--- Scripts/Tail.lua
+++ Scripts/Tail.lua
@@ -1,47 +1,44 @@
 -- Tail
+include("Scripts/Characters/BasePlayer.lua")
 
 -------------------------------------------------------------------------------
-if Tail == nil then
-    Tail = EternusEngine.ModScriptClass.Subclass("Tail")
+
+-- Override model create to hook in adding Tail
+local BasePlayer__SetThirdPersonGameObject = BasePlayer._SetThirdPersonGameObject
+BasePlayer._SetThirdPersonGameObject = function(self, gameObjectName)
+    BasePlayer__SetThirdPersonGameObject(self, gameObjectName)
+
+    self.m_tail = Eternus.GameObjectSystem:NKCreateGameObject("Tail", true)
+    self.m_3pobject:NKAddChildObject(self.m_tail)
+    self.m_tail:NKSetAttachBone("Bn_SpB01")
+    --self.m_3pobject:NKAddChildAtAttachPoint(self.m_tail, "tail", "Bn_SpB01", true)
+    self.m_tail.m_player = self
+    self.m_tail:NKSetPosition(vec3.new(0.0, 0.0, 0.0), false)
+    self.m_tail:NKSetOrientation(quat.new(1.0, 0.0, 0.0, 0.0))
+    self.m_tail:NKScale(1.0, false)
 end
 
 -------------------------------------------------------------------------------
-function Tails:Constructor(  )
-    self.b_assignedTail = false
-    self.b_check = false
-end
 
-function Tails:LocalPlayerReady(player)
-    local gfx = player.m_3pobject:NKGetAnimatedGraphics()
-    gfx:NKSetSubmeshVisibility("Geo_FoxTail", true)
+Tail = BaseCharacter.Subclass("Tail")
 
-    -- Animation
-    player.m_tailAnimSlot = gfx:NKGetBlendSlot("Tail")
+-------------------------------------------------------------------------------
 
-    --WorldService.NKGetLocalWorldPlayer():NKGetPawn().m_3pobject:ApplyRule("Chest", { submesh = "Geo_FoxTail" })
-    --WorldService.NKGetLocalWorldPlayer():NKGetPawn().m_3pobject:SetSlotAppearanceRules("Chest" , { ['Chest'] = { priority = 0.5, submesh = ("Geo_FoxTail") } } )
-
-    --Note for self
-    --Bone weight per vertex matters
-    --Have to assign high weight to Bn_spB01 to prevent deformation
-    --Might be able to counter that with custom animation
-    --Working theory is that because default animations does not have bone animation for our bone structure
-    --Causes it to do weird stuff
-    --TODO create idle animation to see if it combats deformation
-    --Animations can only be defined in game object data file...
-    --Maybe create placeholder game object for tail, set position to player and use GetBonePosition to get specific anchor point
-    self.b_assignedTail = true
-    NKWarn("Assigned Tail")
+function Tail:Constructor( args )
+    self.m_player = nil
 end
 
 -------------------------------------------------------------------------------
--- Called from C++ every time the world is saved
---function Tails:Save(outData)
---end
+function Tail:Update( dt )
+    if not self.m_player:IsDead() then
+        self:NKSetPosition(self.m_player.m_3pobject:NKGetPosition())
+        self:NKSetOrientation(self.m_player.m_3pobject:NKGetOrientation())
+    end
+end
 
--------------------------------------------------------------------------------
--- Called from C++ every time the world is restored
---function Tails:Restore(inData, version)
---end
+function Tail:Spawn()
+    self:NKSetControllerCapsuleSize(1, 1)
+    self:NKEnableScriptProcessing(true)
+end
 
-EntityFramework:RegisterModScript(Tails)
+EntityFramework:RegisterGameObject(Tail)
--- Scripts/Tails.lua
+++ Scripts/Tails.lua
@@ -1,6 +1,4 @@
 -- Tails
-include("Scripts/Characters/3PModel.lua")
-table.insert(ThirdPersonModel.ESlots["Chest"], "Geo_FoxTail")
 
 -------------------------------------------------------------------------------
 if Tails == nil then
@@ -11,29 +9,6 @@ end
 function Tails:Constructor(  )
     self.b_assignedTail = false
     self.b_check = false
-end
-
-function Tails:LocalPlayerReady(player)
-    local gfx = player.m_3pobject:NKGetAnimatedGraphics()
-    gfx:NKSetSubmeshVisibility("Geo_FoxTail", true)
-
-    -- Animation
-    player.m_tailAnimSlot = gfx:NKGetBlendSlot("Tail")
-
-    --WorldService.NKGetLocalWorldPlayer():NKGetPawn().m_3pobject:ApplyRule("Chest", { submesh = "Geo_FoxTail" })
-    --WorldService.NKGetLocalWorldPlayer():NKGetPawn().m_3pobject:SetSlotAppearanceRules("Chest" , { ['Chest'] = { priority = 0.5, submesh = ("Geo_FoxTail") } } )
-
-    --Note for self
-    --Bone weight per vertex matters
-    --Have to assign high weight to Bn_spB01 to prevent deformation
-    --Might be able to counter that with custom animation
-    --Working theory is that because default animations does not have bone animation for our bone structure
-    --Causes it to do weird stuff
-    --TODO create idle animation to see if it combats deformation
-    --Animations can only be defined in game object data file...
-    --Maybe create placeholder game object for tail, set position to player and use GetBonePosition to get specific anchor point
-    self.b_assignedTail = true
-    NKWarn("Assigned Tail")
 end
 
 -------------------------------------------------------------------------------
