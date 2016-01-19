88e684a80d861be46a76b409630880053f62d0f6
Kihira <kihira@kihirakreations.co.uk>
2016-01-19 21:29:55 +0000

    More (failed) attempts at getting the model to even render

--- Scripts/Tail.lua
+++ Scripts/Tail.lua
@@ -1,44 +1,34 @@
 -- Tail
-include("Scripts/Characters/BasePlayer.lua")
+-------------------------------------------------------------------------------
+
+Tail = EternusEngine.GameObjectClass.Subclass("Tail")
 
 -------------------------------------------------------------------------------
 
--- Override model create to hook in adding Tail
-local BasePlayer__SetThirdPersonGameObject = BasePlayer._SetThirdPersonGameObject
-BasePlayer._SetThirdPersonGameObject = function(self, gameObjectName)
-    BasePlayer__SetThirdPersonGameObject(self, gameObjectName)
-
-    self.m_tail = Eternus.GameObjectSystem:NKCreateGameObject("Tail", true)
-    self.m_3pobject:NKAddChildObject(self.m_tail)
-    self.m_tail:NKSetAttachBone("Bn_SpB01")
-    --self.m_3pobject:NKAddChildAtAttachPoint(self.m_tail, "tail", "Bn_SpB01", true)
-    self.m_tail.m_player = self
-    self.m_tail:NKSetPosition(vec3.new(0.0, 0.0, 0.0), false)
-    self.m_tail:NKSetOrientation(quat.new(1.0, 0.0, 0.0, 0.0))
-    self.m_tail:NKScale(1.0, false)
-end
-
--------------------------------------------------------------------------------
-
-Tail = BaseCharacter.Subclass("Tail")
-
--------------------------------------------------------------------------------
-
-function Tail:Constructor( args )
+function Tail:Constructor( )
     self.m_player = nil
 end
 
 -------------------------------------------------------------------------------
+function Tail:PostLoad( )
+    self:NKSetControllerCapsuleSize(10, 10)
+    self:NKEnableScriptProcessing(true)
+    NKWarn("Post load")
+end
+
 function Tail:Update( dt )
-    if not self.m_player:IsDead() then
-        self:NKSetPosition(self.m_player.m_3pobject:NKGetPosition())
-        self:NKSetOrientation(self.m_player.m_3pobject:NKGetOrientation())
+    if self.m_player and not self.m_player:IsDead() then
+        self:NKSetShouldRender(true, true)
+        --self:NKSetPosition(self.m_player.m_3pobject:NKGetPosition())
+        --self:NKSetOrientation(self.m_player.m_3pobject:NKGetOrientation())
+        --NKWarn(self:NKGetPosition():NKToString())
+    else
+        --NKWarn("Player dead")
     end
 end
 
 function Tail:Spawn()
-    self:NKSetControllerCapsuleSize(1, 1)
-    self:NKEnableScriptProcessing(true)
+
 end
 
 EntityFramework:RegisterGameObject(Tail)
--- Scripts/Tails.lua
+++ Scripts/Tails.lua
@@ -1,4 +1,5 @@
 -- Tails
+include("Scripts/Characters/BasePlayer.lua")
 
 -------------------------------------------------------------------------------
 if Tails == nil then
@@ -6,9 +7,37 @@ if Tails == nil then
 end
 
 -------------------------------------------------------------------------------
-function Tails:Constructor(  )
+function Tails:Constructor()
     self.b_assignedTail = false
     self.b_check = false
+end
+
+function Tails:Initialize()
+    Eternus.GameState:RegisterSlashCommand("SpawnTail", self, "SpawnTail")
+end
+
+-- Callback function
+function Tails:SpawnTail(userInput, args, commandName, player)
+    -- Print "Hello world from ExampleMod!" to the (local) chat window
+    local m_tail = Eternus.GameObjectSystem:NKCreateGameObject("Tail", true)
+    m_tail:NKSetPosition(player:NKGetPosition(), false)
+    m_tail:NKSetOrientation(player:NKGetOrientation())
+    NKWarn(m_tail:NKGetPosition():NKToString())
+end
+
+-- Override model create to hook in adding Tail
+local BasePlayer__SetThirdPersonGameObject = BasePlayer._SetThirdPersonGameObject
+BasePlayer._SetThirdPersonGameObject = function(self, ...)
+    BasePlayer__SetThirdPersonGameObject(self, ...)
+
+    self.m_tail = Eternus.GameObjectSystem:NKCreateGameObject("Tail", true)
+    --self.m_3pobject:NKAddChildObject(self.m_tail)
+    --self.m_tail:NKSetAttachBone("Bn_SpB01")
+    --self.m_3pobject:NKAddChildAtAttachPoint(self.m_tail, "tail", "Bn_SpB01", true)
+    self.m_tail.m_player = self
+    self.m_tail:NKSetPosition(vec3.new(0.0, 0.0, 0.0), false)
+    self.m_tail:NKSetOrientation(quat.new(1.0, 0.0, 0.0, 0.0))
+    self.m_tail:NKScale(1.0, false)
 end
 
 -------------------------------------------------------------------------------
@@ -22,3 +51,5 @@ end
 --end
 
 EntityFramework:RegisterModScript(Tails)
+
+-------------------------------------------------------------------------------
